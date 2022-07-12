34f0e784b73879b583d9410265b58bb9a6d684d0
Adrian Irving-Beer <wisq@wisq.net>
2022-07-12 05:21:53 -0400

    `mix new ilcheck`

---
+++ lib/ilcheck.ex
@@ -0,0 +1,18 @@
+defmodule Ilcheck do
+  @moduledoc """
+  Documentation for `Ilcheck`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ilcheck.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ilcheck_test.exs
@@ -0,0 +1,8 @@
+defmodule IlcheckTest do
+  use ExUnit.Case
+  doctest Ilcheck
+
+  test "greets the world" do
+    assert Ilcheck.hello() == :world
+  end
+end
