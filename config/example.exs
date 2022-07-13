eee0a712be2ce2f8a1f6b50ae2e051cda22f5a5a
Adrian Irving-Beer <wisq@wisq.net>
2022-07-13 16:52:30 -0400

    Add example config.

---
+++ config/example.exs
@@ -0,0 +1,44 @@
+# Copy this to config.exs and fill in your data.
+import Config
+
+config :logger, level: :info
+config :logger, :console, format: "$metadata[$level] $message\n"
+
+config :ilcheck,
+  character_name: "YourFull CharacterName",
+  # Classes must be in either long form,
+  #   WAR: %{level: 90, ...},
+  # or in short form:
+  #   WAR: 90,
+  classes: [
+    # Classes with retainers should always come first:
+    WAR: %{level: 90, retainer: "Retainer1Name"},
+    MCH: %{level: 90, retainer: "Retainer2Name"},
+    # Tanks:
+    PLD: 11,
+    # WAR is above
+    DRK: 22,
+    GNB: 33,
+    # Healers:
+    WHM: 44,
+    SCH: 55,
+    AST: 66,
+    SGE: 77,
+    # Melee DPS:
+    MNK: 88,
+    DRG: 12,
+    NIN: 34,
+    SAM: 56,
+    RPR: 78,
+    # Ranged DPS:
+    BRD: 90,
+    # MCH is above
+    DNC: 87,
+    # Magical DPS:
+    BLM: 65,
+    # SMN should be the same as SCH ;)
+    SMN: 55,
+    RDM: 54,
+    # Limited jobs:
+    BLU: 32
+  ]
